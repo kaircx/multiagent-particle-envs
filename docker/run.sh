d1878c258d17f5502459fcd916c63d43bf59b038
kaircx <kaircx@gmail.com>
2023-10-01 16:17:23 +0900

    push

--- docker/run.sh
+++ docker/run.sh
@@ -1,3 +1,3 @@
 SCRIPT_PATH="$(cd "$(dirname "$0")" && pwd -P)"
-docker run -it --privileged --gpus all --net host -v /tmp/.X11-unix:/tmp/.X11-unix -v $SCRIPT_PATH/../:/root/multiagent-particle-envs/ --name particle_env particle_env:latest 
+docker run -it --privileged --gpus all --net host -v /tmp/.X11-unix:/tmp/.X11-unix -v $SCRIPT_PATH/../../:/root/rllab --name particle_env particle_env:latest 
 
